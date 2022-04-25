94971f9db8e496ba5501fe787ef4d4b123a34a7f
riskers <gaoyibobobo@gmail.com>
2022-04-25 21:48:11 +0800

    init

---
+++ src/main/java/com/riskers/githubx/GithubxApplication.java
@@ -0,0 +1,13 @@
+package com.riskers.githubx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riskers/githubx/GithubxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riskers.githubx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
